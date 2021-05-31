864ce1aa672de5da61305f9867681f8af6519519
ankit <ac2000choudhary@gmail.com>
2021-05-31 09:02:41 +0530

    Services Started

--- src/app/components/videos/videos.component.ts
+++ src/app/components/videos/videos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { YoutubeService } from 'src/app/services/youtube.service';
 
 @Component({
   selector: 'app-videos',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private API: YoutubeService) { }
 
   ngOnInit(): void {
+    this.API.getData('/playlists?part=snippet&channelId=UCvFUbROutPnoIgfZrpJ6gNw&key=AIzaSyDdUAIMenXoNLQRA3QWbxxg_DQh2rzMwLo').then((value) => {
+      console.log(value);
+    })
   }
 
 }
